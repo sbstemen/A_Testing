6fd358eecda3df3cb5ba8fe5f418bca125069a73
Scott Stemen <Scott@SkyK9.com>
2018-11-19 13:03:20 -0700

    Saving

---
+++ SubCallTest/CallOne/CallOne/CallOne.cs
@@ -0,0 +1,46 @@
+﻿/* Gotta have a name */
+namespace CallOne
+{
+  using System;
+  using System.Drawing;
+  using System.IO;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using OpenQA.Selenium;
+  using OpenQA.Selenium.Chrome;
+  using UxTestTwo;
+  
+
+
+  [TestClass]
+  public class CallOne
+  {
+    /**/
+    private static string passWord = "123456";
+    private static string testName = "X";
+    private static string runDateTime = DateTime.Now.ToString("-MM-dd-HHmm");
+    private static string logthis = @"C:\TstLog\" + runDateTime + "\\";
+    private static string chromePath = Directory.GetCurrentDirectory() + "\\assets\\";
+    private UxTwo util = new UxTwo(logthis);
+    private PassFailCount results = new PassFailCount();
+    private Usr Admin = new Usr();
+    private Usr NewGuy = new Usr();
+    private CallOneProcess Proc = new CallOneProcess();
+    /**/
+    [TestInitialize]
+    public void TestCount()
+    {
+
+    }
+
+    [TestMethod]
+    public void Test001()
+    {
+      using (IWebDriver DrVr = new ChromeDriver(chromePath))
+      {
+        this.Proc.BrowserReady(DrVr, util, results);
+
+        this.util.RandomPause(3);
+      }
+    }
+  }
+}
---
+++ SubCallTest/CallOne/CallOne/CallOneProcess.cs
@@ -0,0 +1,30 @@
+﻿/* since 1952 */
+
+namespace CallOne
+{
+  using System;
+  using System.Drawing;
+  using System.IO;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using OpenQA.Selenium;
+  using OpenQA.Selenium.Chrome;
+  using UxTestTwo;
+
+  internal class CallOneProcess
+  {
+    public IWebDriver BrowserReady(IWebDriver drvr, UxTwo util, PassFailCount results)
+    {
+      bool testGood = false;
+      string findThis = "Microsoft";
+      string pageText = string.Empty;
+      string startPage = @"https://www.google.com/";
+      Size viewPort = new Size(1650, 1000);
+      drvr.Navigate().GoToUrl(startPage);
+      util.RandomPause(2);
+      drvr.Manage().Window.Size = viewPort;
+      testGood = util.PageWeWanted(util, pageText, findThis, results);
+      return drvr;
+    }
+
+  }
+}
